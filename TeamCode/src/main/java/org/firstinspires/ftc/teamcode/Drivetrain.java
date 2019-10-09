123942feebb92d65b3032c9f9416e405c00b1419
Tyler Smith <tjismith13@icloud.com>
2019-10-09 16:27:43 -0400

    Created full mecanum drive base commands and began encapsulating in subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Hardware;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
+
+public class Drivetrain {
+    private static Drivetrain instance;
+
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+
+    private Drivetrain(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    /**
+     *
+     *
+     * @param forward
+     * @param strafe
+     * @param turn
+     */
+    public void drive(double forward, double strafe, double turn) {
+        double direction = Math.atan2(strafe, forward);
+        double speed = Math.hypot(forward, strafe);
+        double leftBackSpeed = Math.sin(direction - Math.PI / 4) * speed;
+        double rightFrontSpeed = Math.sin(direction - Math.PI / 4) * speed;
+        double leftFrontSpeed = Math.sin(direction + Math.PI / 4) * speed;
+        double rightBackSpeed = Math.sin(direction + Math.PI / 4) * speed;
+
+        leftBack.setPower(leftBackSpeed);
+        rightBack.setPower(rightBackSpeed);
+        leftFront.setPower(leftFrontSpeed);
+        rightFront.setPower(rightFrontSpeed);
+
+        double fac1 = max(abs(leftBackSpeed), abs(rightBackSpeed));
+        double fac2 = max(abs(leftFrontSpeed), abs(rightFrontSpeed));
+        double speedScalingFactor = max(fac1, fac2);
+
+        if (turn < 0) {
+            leftBack.setPower(leftBackSpeed + turn);
+            leftFront.setPower(leftFrontSpeed + turn);
+            rightBack.setPower(leftBackSpeed - turn);
+            rightFront.setPower(rightFrontSpeed - turn);
+            if (leftBackSpeed + turn < -1 || leftFrontSpeed + turn < -1) {
+                leftBack.setPower((leftBackSpeed + turn) / speedScalingFactor);
+                leftFront.setPower((leftFrontSpeed + turn) / speedScalingFactor);
+                rightBack.setPower((rightBackSpeed + turn) / speedScalingFactor);
+                rightFront.setPower((rightFrontSpeed + turn) / speedScalingFactor);
+            }
+            if (rightBackSpeed + turn > 1 || rightFrontSpeed + turn > 1) {
+                leftBack.setPower((leftBackSpeed + turn) / speedScalingFactor);
+                leftFront.setPower((leftFrontSpeed + turn) / speedScalingFactor);
+                rightBack.setPower((rightBackSpeed + turn) / speedScalingFactor);
+                rightFront.setPower((rightFrontSpeed + turn) / speedScalingFactor);
+            }
+        }
+        else if (turn > 0) {
+            rightBack.setPower(rightBackSpeed - turn);
+            rightFront.setPower(rightFrontSpeed - turn);
+            leftBack.setPower(leftBackSpeed + turn);
+            leftFront.setPower(leftFrontSpeed + turn);
+            if (leftBackSpeed + turn < -1 || leftFrontSpeed + turn < -1) {
+                leftBack.setPower((leftBackSpeed + turn) / speedScalingFactor);
+                leftFront.setPower((leftFrontSpeed + turn) / speedScalingFactor);
+                rightBack.setPower((rightBackSpeed + turn) / speedScalingFactor);
+                rightFront.setPower((rightFrontSpeed + turn) / speedScalingFactor);
+            }
+            if (rightBackSpeed + turn > 1 || rightFrontSpeed + turn > 1) {
+                leftBack.setPower((leftBackSpeed + turn) / speedScalingFactor);
+                leftFront.setPower((leftFrontSpeed + turn) / speedScalingFactor);
+                rightBack.setPower((rightBackSpeed + turn) / speedScalingFactor);
+                rightFront.setPower((rightFrontSpeed + turn) / speedScalingFactor);
+            }
+        }
+    else {
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+    }
+}
+    public static Drivetrain getInstance(HardwareMap hardwareMap, Telemetry telemetry) {
+        if(instance == null) {
+            instance = new Drivetrain(hardwareMap, telemetry);
+        }
+
+        return instance;
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -6,64 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
+import static java.lang.Math.toIntExact;
+
 @TeleOp(name = "MecanumTest")
 public class MecanumTest extends OpMode {
 
-    private DcMotor leftFront;
-    private DcMotor rightFront;
-    private DcMotor leftBack;
-    private DcMotor rightBack;
+    private Drivetrain drivetrain;
 
     @Override
     public void init() {
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        telemetry.addData("Left front mode", leftFront.getMode());
+        drivetrain = Drivetrain.getInstance(hardwareMap, telemetry);
     }
 
     @Override
     public void loop() {
-        if (gamepad1.left_stick_x != 0 || gamepad1.left_stick_y != 0) {
-
-            double y = gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double theta = Math.atan2(y, x);
-            double leftBackSpeed = 0;
-            double direction = theta;
-            double speed = Math.hypot(x, y);
-
-            double rightFrontSpeed = 0;
-
-            leftBackSpeed = Math.sin(direction - Math.PI / 4) * speed;
-            rightFrontSpeed = Math.sin(direction - Math.PI / 4) * speed;
-
-            double leftFrontSpeed = 0;
-
-            double rightBackSpeed = 0;
-
-            leftFrontSpeed = Math.sin(direction + Math.PI / 4) * speed;
-            rightBackSpeed = Math.sin(direction + Math.PI / 4) * speed;
-
-            leftBack.setPower(leftBackSpeed);
-            rightBack.setPower(rightBackSpeed);
-            leftFront.setPower(leftFrontSpeed);
-            rightFront.setPower(rightFrontSpeed);
-
-            telemetry.addData("angle", theta);
-            telemetry.addData("FL=", leftFrontSpeed);
-            telemetry.addData("FR=", rightFrontSpeed);
-            telemetry.addData("BL=", leftBackSpeed);
-            telemetry.addData("BR=", rightBackSpeed);
-        } else {
-            leftFront.setPower(0);
-            rightFront.setPower(0);
-            leftBack.setPower(0);
-            rightBack.setPower(0);
-        }
+        drivetrain.drive(gamepad1.left_stick_y, -gamepad1.left_stick_x, gamepad1.right_stick_x);
     }
 }
