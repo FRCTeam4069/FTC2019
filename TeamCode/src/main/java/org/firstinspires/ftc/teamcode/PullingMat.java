3abd9c3cf32408cfa14d3242b8fd7804238d11b1
Rhys Kenwell <redrield@gmail.com>
2019-11-13 14:42:29 -0500

    Work on gyro.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PullingMat.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaSkyStone;
+
+@Autonomous
+public class PullingMat extends LinearOpMode{
+
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            int leftFrontPos = leftFront.getCurrentPosition();
+            int rightFrontPos = rightFront.getCurrentPosition();
+            int leftBackPos = leftBack.getCurrentPosition();
+            int rightBackPos = rightBack.getCurrentPosition();
+
+            double averageMotorPosition = (leftFrontPos + rightFrontPos + leftBackPos + rightBackPos) / 4.0;
+            double setpoint = 2000;
+            double originalSetpoint = -2000;
+            double error = setpoint - averageMotorPosition;
+            double reversedError = originalSetpoint - averageMotorPosition;
+            double kP = 0.01;
+            double currentTime = System.currentTimeMillis();
+
+            leftFront.setPower(kP * error);
+            rightFront.setPower(kP * error);
+            leftBack.setPower(kP * error);
+            rightBack.setPower(kP * error);
+
+            if (currentTime >= 20000) {
+                leftFront.setPower(kP * reversedError);
+                rightFront.setPower(kP * reversedError);
+                leftBack.setPower(kP * reversedError);
+                rightBack.setPower(kP * reversedError);
+            }
+
+            if (currentTime >= 40000) {
+                leftFront.setPower(kP * error);
+                rightFront.setPower(kP * reversedError);
+                leftBack.setPower(kP * error);
+                rightBack.setPower(kP * reversedError);
+            }
+
+
+
+
+
+
+        }
+    }
+}
