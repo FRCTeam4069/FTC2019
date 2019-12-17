5e095776344c7f0c127ba3e00e10bcc26c353524
bfmat <brendon-m@outlook.com>
2019-12-16 20:32:44 -0800

    Elevator state machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
@@ -4,6 +4,7 @@ import com.acmerobotics.roadrunner.profile.MotionProfile;
 import com.acmerobotics.roadrunner.profile.MotionProfileBuilder;
 import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -17,37 +18,59 @@ public class Elevator {
 
 //    private CRServo Clamp;
 //    private CRServo Wrench;
-    private CRServo Elevator;
+    private CRServo servo;
+    private ColorSensor colorSensor;
     Telemetry telemetry;
-    MotionProfile currentProfile = null;
 
     public Elevator(HardwareMap hardwareMap, Telemetry telemetry) {
 //        Clamp = hardwareMap.get(CRServo.class, "Clamp");
 //        Wrench = hardwareMap.get(CRServo.class, "Wrench");
-        Elevator = hardwareMap.get(CRServo.class, "Elevator");
+        servo = hardwareMap.get(CRServo.class, "Elevator");
 
         this.telemetry = telemetry;
+
+        colorSensor = hardwareMap.colorSensor.get("color");
     }
 
-    public void setPosition(double position) {
-
+    private enum ElevatorMode {
+        LIMIT_SWITCH,
+        GREEN_SENSOR,
+        MANUAL_DOWN,
+        MANUAL_UP,
+        STOP
 
     }
 
-    public void update(boolean forward, boolean reverse, double power) {
-        if (forward) {
-//            Wrench.setPower(1);
-        } else if (reverse) {
-//            Wrench.setPower(-1);
+    private ElevatorMode mode = ElevatorMode.STOP;
+
+    public void setMode(ElevatorMode mode) {
+        this.mode = mode;
+    }
+
+    public void update() {
+
+        double power;
+
+        if (mode == ElevatorMode.LIMIT_SWITCH) {
+            power = -0.5;
+            // CODE TO CHECK LIMIT SWITCH
+        } else if (mode == ElevatorMode.GREEN_SENSOR) {
+            power = 0.5;
+            if (colorSensor.green() > 128) {
+                mode = ElevatorMode.STOP;
+            }
+        } else if (mode == ElevatorMode.MANUAL_UP) {
+            power = 0.5;
+        } else if (mode == ElevatorMode.MANUAL_DOWN) {
+            power = -0.5;
         } else {
-//
-//            Wrench.setPower(0);
+            power = 0;
         }
 
-        Elevator.setPower(power);
-
+        servo.setPower(power);
     }
 }
+
 
 
 
